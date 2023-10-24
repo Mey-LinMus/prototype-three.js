3792691c8fa39ccaf645ccf65785557c9b785c1b
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-24 13:30:49 +0200

    Installing drei package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "../style/App.css"
 
 function App() {
   const [file, setFile] = useState(null);
   const [fileUrl, setFileUrl] = useState("");
   const [filesList, setFilesList] = useState([]);
 
+  const onFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  
   return <div>
-    
+
   </div>;
 }
 
