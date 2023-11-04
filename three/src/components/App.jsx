aa153fb5d5fda2fc4764aecbd0e680483ffbfd3b
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-11-04 17:21:17 +0100

    Making albums

--- three/src/components/App.jsx
+++ three/src/components/App.jsx
@@ -4,17 +4,31 @@ import { OrbitControls, Stars } from "@react-three/drei";
 import Modelfile from "./Models";
 import "../App.css";
 
-// https://3dviewer.net/
-
 const App = () => {
   const [selectedModel, setSelectedModel] = useState(null);
   const [loadingError, setLoadingError] = useState(null);
+  const [selectedAlbum, setSelectedAlbum] = useState(null);
 
-  const models = [
-    { name: "Portal", path: "/spiders_portal.glb" },
-    { name: "Spider-man", path: "/spider-man.glb" },
-    { name: "Cat", path: "/cat.glb" },
-    { name: "Spider-man 2099", path: "/spider-man_2099_.glb" },
+  const albums = [
+    {
+      name: "Spiderman Album",
+      models: [
+        { name: "Portal", path: "/spiders_portal.glb" },
+        { name: "Spider-man", path: "/spider-man.glb" },
+        { name: "Spider-man 2099", path: "/spider-man_2099_.glb" },
+      ],
+    },
+    {
+      name: "Animal Album",
+      models: [
+        { name: "Cat", path: "/cat.glb" },
+        { name: "Turtle", path: "/turtle.glb" },
+        { name: "Mink", path: "/mink.glb" },
+        { name: "leopard", path: "leopard.glb" },
+      
+      ],
+    },
+    // Add more albums as needed
   ];
 
   const loadModel = (modelPath) => {
@@ -24,16 +38,28 @@ const App = () => {
 
   return (
     <div>
-      <div className="button-container">
-        {models.map((model, index) => (
+      <div className="album-selector">
+        {albums.map((album, index) => (
           <button
             key={index}
-            className="button"
-            onClick={() => loadModel(model.path)}
+            className="album-button"
+            onClick={() => setSelectedAlbum(album)}
           >
-            {model.name}
+            {album.name}
           </button>
         ))}
+      </div>
+      <div className="model-selector">
+        {selectedAlbum &&
+          selectedAlbum.models.map((model, index) => (
+            <button
+              key={index}
+              className="model-button"
+              onClick={() => loadModel(model.path)}
+            >
+              {model.name}
+            </button>
+          ))}
       </div>
       <Canvas style={{ position: "absolute" }}>
         <OrbitControls />
